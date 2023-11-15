2d1bcccc41d74524a7745583df0dffaf9df4b1d1
Yuxiao Wang <rachel@YuxiaodeMBP.lan>
2023-11-15 13:07:55 -0600

    Add static files

--- myproject/settings.py
+++ myproject/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -121,5 +122,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
